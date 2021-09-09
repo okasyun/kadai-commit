bf5036922ae3971cc4352171b631f4b7978ef206
shun.okamoto <shunshun1207shun@gmail.com>
2021-09-09 22:21:02 +0000

    add omikuji function

--- sample.php
+++ sample.php
@@ -1,5 +1,13 @@
 <?php
     $result = '無し';
+    if (array_key_exists('act', $_POST)) {
+        $result = omikuji(); //戻り値　returnがここにくる   
+    }
+    
+    function omikuji() {
+        $furtune = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"];
+        return $fortune[random_int(0, count($fortune) -1)];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
